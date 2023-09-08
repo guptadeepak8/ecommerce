57ad716fc469e1138d0fc89d50a48046807fbb2c
guptadeepak8 <vectord7738@gmail.com>
2023-09-08 14:31:04 +0530

    updated

--- server/service/common.js
+++ server/service/common.js
@@ -9,7 +9,6 @@ exports.cookieExtractor = function(req) {
   if (req && req.cookies) {
       token = req.cookies['jwt'];
   }
-  token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZmFkYmNlZTY3ZDhmOGFjNmMwNzg5OSIsImlhdCI6MTY5NDE2MTg3MX0.BXL4dZjRniHV3pWXOiVQ7_VPBXCLpv8LUOPkkfGI0g8"
  
   return token;
 };
